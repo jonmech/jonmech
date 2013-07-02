82a7205b753425efe6c9acc7e2bd1f19024ed449
jonmech <jonmech@gmail.com>
2013-07-02 13:10:24 +0430

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jonmech::Application.config.secret_key_base = 'de22aa96e736fa996d2d4a61ac38746317fe4e7deeb7223e495379c279f402a622d684da474ffd5c1918cb2053a5b0c919b3bccc47d26c8f0839766c487cd9c1'
